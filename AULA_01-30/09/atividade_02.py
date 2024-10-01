b10e9e8704b8f5055151e1ed68ab39428c6acc97
deiseogfiuza <deiseoglima86@gmail.com>
2024-10-01 18:45:35 -0300

    Add files via upload

---
+++ AULA_01-30/09/atividade_02.py
@@ -0,0 +1,5 @@
+# Cálculo de Idade com base de ano e inclusão de Nome
+nome = input("Informe seu Nome : ")
+base = int(input("Informe o seu Ano de Nascimento : "))
+calculo = 2024 - base 
+print(nome,"Sua idade é : " ,calculo) 
---
+++ AULA_01-30/09/exemplo_01.py
@@ -0,0 +1,3 @@
+nome = "Deise" # String
+idade = 38 # Integer
+n2 = 5.2 #Float
