69cc841e948d7e7b274bdbaee1cfcb5376db157a
deiseogfiuza <deiseoglima86@gmail.com>
2024-10-04 21:33:35 -0300

    Add files via upload

---
+++ AULA_05-04/10/atividade_16.py
@@ -0,0 +1,8 @@
+print("Usando o comando For .....")
+for i in range (5) :
+    print(i) 
+print("Usando o comando While .....")
+i = 1
+while i <= 5 :
+    print(i)
+    i += 1
---
+++ AULA_05-04/atividade_17.py
@@ -0,0 +1,5 @@
+resp = "S"
+while resp == "S":
+    nome = input("Informe o nome de um usuário: ")
+    resp = input("Deseja Continjuar(S/N)?")
+print(f"Obrigado pela Participação ! {nome} , {nome2} e {nome3} ")
